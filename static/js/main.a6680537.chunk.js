0d1bffa1cdb374708fad6ea1e454c67d67b3c6da
Sergey-Markov <1nativefl@gmail.com>
2021-08-28 17:41:45 +0300

    Updates

--- static/js/main.a6680537.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=(n(8),n(0));var p=function(){return Object(s.jsx)("div",{className:"App",children:"my App"})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(p,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.a6680537.chunk.js.map
